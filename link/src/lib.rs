c7a127667dc8bfd26d41cc8d1f9b7d5fee07924c
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-13 04:18:45 +0100

    Update link (test)

--- link/src/lib.rs
+++ link/src/lib.rs
@@ -5,9 +5,11 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, link!");
+    log("Hello, world!");
 }
