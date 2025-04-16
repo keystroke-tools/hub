160a604ffbbfd5f8b4a4ea21eab85dda8ec843e1
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-04-16 04:52:23 +0100

    Use new request host FN

--- link/src/lib.rs
+++ link/src/lib.rs
@@ -1,5 +1,5 @@
 use dom_smoothie::{Article, Config, Readability, TextMode};
-use hubble::{error::Error, types::entry};
+use hubble::{error::Error, types};
 
 /// The main function for the `on_create` event.
 ///
@@ -7,7 +7,7 @@ use hubble::{error::Error, types::entry};
 /// This function is marked as `unsafe` because it interacts with "raw" memory pointers.
 #[cfg_attr(all(target_arch = "wasm32"), unsafe(export_name = "on_create"))]
 pub unsafe extern "C" fn _on_create(ptr: u32, len: u32) {
-    match hubble::types::Entry::read_from_capnp(ptr, len) {
+    match hubble::types::Entry::read_from_memory(ptr, len) {
         Ok(entry) => {
             if let Err(e) = on_create(entry) {
                 hubble::log::error(&format!("Error processing entry: {:?}", e));
@@ -19,7 +19,7 @@ pub unsafe extern "C" fn _on_create(ptr: u32, len: u32) {
     }
 }
 
-fn on_create(entry: entry::Entry) -> Result<(), Error> {
+fn on_create(entry: types::Entry) -> Result<(), Error> {
     let parsed_url = url::Url::parse(&entry.url).map_err(|e| Error::PluginError(e.to_string()))?;
     let host = parsed_url
         .host_str()
@@ -27,12 +27,38 @@ fn on_create(entry: entry::Entry) -> Result<(), Error> {
     let scheme = parsed_url.scheme();
     let base_url = format!("{}://{}", scheme, host);
 
-    // let mut readability = Readability::new(html, document_url, cfg)
+    let _cfg = Config {
+        max_elements_to_parse: 10_000,
+        text_mode: TextMode::Markdown,
+        ..Default::default()
+    };
+
+    let resp = hubble::request(types::RequestOpts {
+        method: types::NetworkMethod::Get,
+        url: entry.url.clone(),
+        headers: None,
+        body: None,
+    })?;
+    // Convert vector to string
+    let body = String::from_utf8(resp.body).map_err(|e| Error::PluginError(e.to_string()))?;
+
+    hubble::log::debug(&format!(
+        "Response: {{ base_url: {}, code: {}, body: {} }}",
+        base_url, resp.status_code, body
+    ));
+
+    // let mut readability = Readability::new(html, Some(&base_url), Some(cfg))
+    //     .map_err(|e| Error::PluginError(e.to_string()))?;
+    //
+    // let article: Article = readability
+    //     .parse()
+    //     .map_err(|e| Error::PluginError(format!("Failed to parse article: {}", e)))?;
 
     // Read the Cap'n Proto message from the provided pointer and length.
-    hubble::log::debug(&format!(
-        "Entry {{ link: {}, base_url: {} }}",
-        entry.url, base_url
-    ));
+    // hubble::log::debug(&format!(
+    //     "Entry {{ title: {}, byline: {} }}",
+    //     article.title,
+    //     article.byline.unwrap_or_default()
+    // ));
     Ok(())
 }
